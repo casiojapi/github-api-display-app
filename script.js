1cf90cb46dd4303f3c69469f295d8d947ebaf544
viieja <msantoro@fi.uba.ar>
2020-12-13 21:39:12 -0300

    fix links and comments

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ async function get_user(username) {
 }
 
 async function get_repos(username) {
+
     const resp = await fetch(APIURL + username +  "/repos");
     const resp_data = await resp.json();
 
@@ -26,49 +27,42 @@ function create_user_card(user) {
     card.classList.add('card');
 
     const cardHTML = `
-        <div class="card">
-            <div>
-                <a target="_blank" href="https://github.com/${user.login}"><img class="avatar" src="${user.avatar_url}" alt="${user.name}" /></a>
-            </div>
-            <div class="user-info">
-                <a target="_blank" href="https://github.com/${user.login}"><h2>${user.name}</h2></a>
-                <p>${user.bio}</p>
+            <div class="card">
+                <div>
+                    <a target="_blank" href="https://github.com/${user.login}"><img class="avatar" src="${user.avatar_url}" alt="${user.name}" /></a>
+                </div>
+                <div class="user-info">
+                    <a target="_blank" href="https://github.com/${user.login}"><h2>${user.name}</h2></a>
+                    <p>${user.bio}</p>
 
-                <ul class="info">
-                    <li>${user.followers} followers</li>
-                    <li>${user.following} following</li>
-                    <li>${user.public_repos} repos</li>
-                </ul>
-                <div id="repos"></div>
+                    <ul class="info">
+                        <li>${user.followers} followers</li>
+                        <li>${user.following} following</li>
+                        <li>${user.public_repos} repos</li>
+                    </ul>
+                    <div id="repos"></div>
+                </div>
             </div>
-        </div>
     `;
     main.innerHTML = cardHTML;
 }
 
 function add_repos_to_card(repos) {
-    const repos_div= document.getElementById("repos");
-    console.log(repos);
-    repos.forEach(repo => {
-        // if (repo.homepage) { // just the ones with github pages (or webpage)
-            const repo_elem = document.createElement('a');
+    const repos_div = document.getElementById("repos");
 
+    repos.forEach(repo => {
+            const repo_elem = document.createElement('a');
             repo_elem.classList.add("repo");
-            repo_elem.href = repo.homepage;
+            repo_elem.href = `https://github.com/${repo.full_name}`;
             repo_elem.target = "_blank";
             repo_elem.innerText = repo.name;
-
-
             repos_div.appendChild(repo_elem);
-        // }
-    })
+    });
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const user = search.value;
-
     if (user) {
         get_user(user);
         search.value = "";
